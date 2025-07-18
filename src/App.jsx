66e00e9ed90763b654882b00cbe2dc51d2317e89
ribello <ofbg01@hotmail.com>
2025-07-18 13:49:23 +0200

    Actualizar API Key

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const firebaseConfig = {
   appId: "1:747549095010:web:cdf74ecc79087715080d3b"
 };
 
-const GOOGLE_AI_API_KEY = "AIzaSyBurU72YTk7lSkX5pi6HBKSZnUcjl1nbC8";
+const GOOGLE_AI_API_KEY = "AIzaSyCAIisxYlJM9lHrUfTGeALv30-UplNw4Ys";
 
 // --- COMPONENTES DE LA INTERFAZ ---
 
